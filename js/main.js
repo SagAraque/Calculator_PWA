e39656c27d619cf9ccd87be19c23faa116dcfc0e
SagAraque <sagaraque@gmail.com>
2021-12-09 00:22:32 +0100

    Calculadora VUE V1.1
    Fixed an error with multiplication that caused the operation result to be an error

--- js/main.js
+++ js/main.js
@@ -8,14 +8,19 @@ const app = Vue.createApp({
     methods: {
         setCharacter(value) {
             if(this.operation.length!=15){
-                this.operation === '0' ? this.operation=value : this.operation=this.operation+value;
+                this.operation === '0' || this.operation === 'Error' ? this.operation=value : this.operation=this.operation+value;
             }
         },
         restart() {
             this.operation = '0';
         },
         operate(){
-            this.operation=eval(this.operation);
+            try {
+                this.operation=eval(this.operation.replace('x','*')).toFixed(6); 
+            } catch (error) {
+                this.operation='Error';
+            }
+            if(this.operation === undefined) this.operation='Error';
         }
     },
 });
